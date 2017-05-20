5e62d374d43e90e50bdfc1da0a6cb5ef1f7f7ff5
richielybmp <richielybmp@hotmail.com>
2017-05-19 21:55:24 -0300

    Validação ao Model

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -8,6 +8,7 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
@@ -15,10 +16,18 @@ namespace MvcMovie.Models
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
