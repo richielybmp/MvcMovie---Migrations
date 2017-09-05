14240755b15e35420d1bea0988fc76ac9519c15b
richielybmp <richielybmp@hotmail.com>
2017-09-04 21:22:06 -0300

    Atualização das labels e adição do menu Lista de Filmes

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,26 +6,30 @@ namespace MvcMovie.Models
 {
     public class Movie
     {
+        [Display(Name = "Código")]
         public int ID { get; set; }
 
+        [Display(Name = "Título")]
         [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
+        [Display(Name = "Data de lançamento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
-
+        [Display(Name = "Data de lançamento")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         [Required]
         [StringLength(30)]
         public string Genre { get; set; }
 
+        [Display(Name = "Preço")]
         [Range(1, 100)]
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [Display(Name = "Avaliação")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         [StringLength(5)]
         public string Rating { get; set; }
